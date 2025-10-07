6c5c197b489d19565feb6213a80379550d7269c5
claudiagarciagayosso <claudia.garcia.gayosso@gmail.com>
2025-10-07 17:26:31 +0000

    Datos

--- DBconfiguration/save_data.sql
+++ DBconfiguration/save_data.sql
@@ -1,7 +1,9 @@
--- Insertar datos en la tabla usuarios 
-INSERT INTO usuarios (nombre, correo, telefono, fecha_nacimiento) VALUES 
-('Juan Pérez', 'juan.perez1@example.com', '1234567890', '1985-01-15');
+INSERT INTO usuarios (id_usuario, nombre, correo, telefono, fecha_nacimiento) VALUES
+(1, 'Juan Pérez', 'juan.perez@example.com', '777-111-2233', '1990-03-15'),
+(2, 'María López', 'maria.lopez@example.com', '777-444-5566', '1992-08-21'),
+(3, 'Carlos Ruiz', 'carlos.ruiz@example.com', '777-777-8899', '1988-12-05');
 
--- Insertar datos en la tabla credenciales 
-INSERT INTO credenciales (id_usuario, username, password_hash) VALUES 
-(1, 'juan.perez1', 'hash_juan_perez');
+INSERT INTO credenciales (id_credencial, id_usuario, username, password_hash) VALUES
+(1, 1, 'juan.perez1', 'hash_juan_perez'),
+(2, 2, 'maria.lopez2', 'hash_maria_lopez'),
+(3, 3, 'carlos.ruiz3', 'hash_carlos_ruiz');
